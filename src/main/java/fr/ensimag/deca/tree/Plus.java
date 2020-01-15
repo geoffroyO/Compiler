dfe8081149d06ef8b8a3bff2b44aa66fb745ab71
Lucas Morin <Lucas.Morin@grenoble-inp.org>
2020-01-15 11:33:22 +0100

    merge

--- src/main/java/fr/ensimag/deca/tree/Plus.java
+++ src/main/java/fr/ensimag/deca/tree/Plus.java
@@ -5,7 +5,6 @@ import fr.ensimag.deca.DecacCompiler;
 import fr.ensimag.ima.pseudocode.GPRegister;
 import fr.ensimag.ima.pseudocode.Register;
 import fr.ensimag.ima.pseudocode.instructions.ADD;
-import fr.ensimag.ima.pseudocode.instructions.FLOAT;
 import fr.ensimag.ima.pseudocode.instructions.POP;
 import fr.ensimag.ima.pseudocode.instructions.PUSH;
 import fr.ensimag.ima.pseudocode.instructions.LOAD;
@@ -28,45 +27,27 @@ public class Plus extends AbstractOpArith {
     protected void codeGenExpr(DecacCompiler compiler, GPRegister register){
         // TODO factoriser le code?
         if (compiler.getRegM().hasFreeGPRegister()) {
-            GPRegister regLeftOp = compiler.getRegM().findFreeGPRegister();
+            GPRegister reg_left_op = compiler.getRegM().findFreeGPRegister();
 
-            this.getLeftOperand().codeGenExpr(compiler, regLeftOp);
+            this.getLeftOperand().codeGenExpr(compiler, reg_left_op);
             this.getRightOperand().codeGenExpr(compiler, register);
+
+            compiler.addInstruction(new ADD(reg_left_op, register));
+            compiler.getRegM().freeRegister(reg_left_op);
             
-            if (!this.getLeftOperand().getType().sameType(this.getLeftOperand().getType()) ) {
-            	if (getLeftOperand().getType().isInt()) {		
-            		//TODO push pop
-            		GPRegister newRegLeftOp = compiler.getRegM().findFreeGPRegister();
-            		
-            		compiler.addInstruction(new FLOAT(regLeftOp, newRegLeftOp));
-            		compiler.addInstruction(new ADD(newRegLeftOp, register));
-            		compiler.getRegM().freeRegister(newRegLeftOp);
-            	}
-            	else {           		
-            		//TODO push pop
-            		GPRegister newRegister = compiler.getRegM().findFreeGPRegister();
-            		
-            		compiler.addInstruction(new FLOAT(register, newRegister));
-            		compiler.addInstruction(new ADD(regLeftOp, newRegister));
-            		compiler.getRegM().freeRegister(newRegister);            		
-            	}
-            }            
-            compiler.addInstruction(new ADD(regLeftOp, register));
-            compiler.getRegM().freeRegister(regLeftOp);
         } else {
-            GPRegister regLeftOp = Register.getR(compiler.getRegM().getNb_registers());
+            GPRegister reg_left_op = Register.getR(compiler.getRegM().getNb_registers());
 
             this.getRightOperand().codeGenExpr(compiler, register);
 
-            compiler.addInstruction(new PUSH(regLeftOp));
+            compiler.addInstruction(new PUSH(reg_left_op));
 
-            this.getLeftOperand().codeGenExpr(compiler, regLeftOp);
+            this.getLeftOperand().codeGenExpr(compiler, reg_left_op);
 
-            compiler.addInstruction(new LOAD(regLeftOp, Register.R0));
-            compiler.addInstruction(new POP(regLeftOp));
+            compiler.addInstruction(new LOAD(reg_left_op, Register.R0));
+            compiler.addInstruction(new POP(reg_left_op));
 
             compiler.addInstruction(new ADD(Register.R0, register));
         }
-        
     }
 }
