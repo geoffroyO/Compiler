1f9a718f62ff0349d8da2a7edb877e338c9c10b1
Majd Odeh <majd.odeh@grenoble-inp.org>
2020-01-17 10:31:37 +0100

    [Added] example_without_objects.deca

--- src/test/deca/global/all.sh
+++ src/test/deca/global/all.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-FILE="affectation1"
+FILE="example_without_objects"
 echo -e "\ntest_lex -----------------------------"
 test_lex $FILE".deca"
 echo -e "\ntest_synt -----------------------------"
@@ -9,5 +9,7 @@ echo -e "\ntest_context -----------------------------"
 test_context $FILE".deca"
 echo -e "\ndecac -----------------------------"
 decac $FILE".deca"
+echo -e "\ndecac -p -----------------------------"
+decac -p $FILE".deca"
 echo -e "\nima -----------------------------"
 ima $FILE.ass
