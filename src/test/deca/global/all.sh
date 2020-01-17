9475d141860ec4b6d7d5fc1fbb6ff7498f0e08e8
Majd Odeh <majd.odeh@grenoble-inp.org>
2020-01-17 13:24:17 +0100

    [Updated] .gitignore and all.sh script

--- src/test/deca/global/all.sh
+++ src/test/deca/global/all.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
 FILE="example_without_objects"
+read -n 1 -s -r -p " Press any key to start"
 echo -e "\ntest_lex -----------------------------"
 test_lex $FILE".deca"
+read -n 1 -s -r -p " Press any key to continue"
 echo -e "\ntest_synt -----------------------------"
 test_synt $FILE".deca"
+read -n 1 -s -r -p " Press any key to continue"
 echo -e "\ntest_context -----------------------------"
 test_context $FILE".deca"
+read -n 1 -s -r -p " Press any key to continue"
 echo -e "\ndecac -----------------------------"
 decac $FILE".deca"
+read -n 1 -s -r -p " Press any key to continue"
 echo -e "\ndecac -p -----------------------------"
 decac -p $FILE".deca"
+read -n 1 -s -r -p " Press any key to continue"
 echo -e "\nima -----------------------------"
 ima $FILE.ass
+read -n 1 -s -r -p " Press any key to exit"
