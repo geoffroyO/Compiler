1e19ef644bf8a6c936ec9bad791988e1196a9da3
Jérémy Navarro <jeremy.navarro@grenoble-inp.org>
2020-01-21 10:47:23 +0100

    Selection, MethodCall, ...

--- src/main/java/fr/ensimag/deca/tree/This.java
+++ src/main/java/fr/ensimag/deca/tree/This.java
@@ -1,5 +1,38 @@
 package fr.ensimag.deca.tree;
 
-public class This {
+import fr.ensimag.deca.DecacCompiler;
+import fr.ensimag.deca.context.ClassDefinition;
+import fr.ensimag.deca.context.ContextualError;
+import fr.ensimag.deca.context.EnvironmentExp;
+import fr.ensimag.deca.context.Type;
+import fr.ensimag.deca.tools.IndentPrintStream;
 
+import java.io.PrintStream;
+
+public class This extends AbstractExpr{
+
+    public This()
+    {
+
+    }
+
+    @Override
+    public Type verifyExpr(DecacCompiler compiler, EnvironmentExp localEnv, ClassDefinition currentClass) throws ContextualError {
+        return null;
+    }
+
+    @Override
+    public void decompile(IndentPrintStream s) {
+        s.print("this");
+    }
+
+    @Override
+    protected void prettyPrintChildren(PrintStream s, String prefix) {
+        s.print(prefix + " This");
+    }
+
+    @Override
+    protected void iterChildren(TreeFunction f) {
+
+    }
 }
