bc4e5e8b97ce6ae03cf9710e5c4731453ee1692d
Jérémy Navarro <jeremy.navarro@grenoble-inp.org>
2020-01-25 10:53:23 +0100

    Add test

--- src/test/script/internalErrors.sh
+++ src/test/script/internalErrors.sh
@@ -3,36 +3,32 @@
 ## Souleve des erreurs interne du compilateur
 
 ## Les tests sont contenus dans le fichier internal error
-## Chaque sous dossier contient des fichiers .deca ainsi que error.find 
+## Chaque sous dossier contient des fichiers .deca ainsi que error.find
 ## qui contient le type d'erreur soulevee
 ## Deux fichiers sont generes a chaque fois dans les LOGS
 ## Un fichier contenant la sortie <type><heure>.output
 ## Un fichier contenant les erreurs <type><heure>.error
 
-## En cas de reussite les fichier de logs sont supprimes 
+## En cas de reussite les fichier de logs sont supprimes
 ## Si une erreur est detecte, le script s'arrête avec un exit 1
 
 ## Les fichiers doivent avoir une sortie vide
 ## Les fichiers doivent contenir l'erreur dans le log d'erreur
-
 
 GREEN='\033[0;32m'
 RED='\033[0;31m'
 BLUE='\033[0;34m'
 COLORLESS='\033[0m'
 
-pFailure()
-{
+pFailure() {
     echo -e "$RED$1$COLORLESS"
 }
 
-pSuccess()
-{
+pSuccess() {
     echo -e "$GREEN$1$COLORLESS"
 }
 
-pInfo()
-{
+pInfo() {
     echo -e "$BLUE$1$COLORLESS"
 }
 
@@ -51,55 +47,61 @@ mkdir $LOGS #Create LOGS folder
 # On se place dans le repertoire de test
 cd $pathRootProject$pathTests
 
-for i in $(find . -maxdepth 1 -mindepth 1 -type d)
-# Iterate over the folder
-do
+for i in $(
+    # Iterate over the folder
+    find . -maxdepth 1 -mindepth 1 -type d
+); do
 
     cd $i
 
-    typeError=$(cat error.find)
+    errorToFind=$(cat error.find)
 
-    echo 
-    pInfo "$typeError"
+    echo
+    pInfo "$errorToFind"
     echo
 
-    for v in $(find . -name "*.deca")
-    do
+    for v in $(find . -name "*.deca"); do
 
-        cTime="`date "+%m_%d_%H_%M_%S"`"
-        nameFile=$v.$cTime
+        cTime="$(date "+%m_%d_%H_%M_%S")"
 
-        decac $v > $LOGS/$nameFile.output 2> $LOGS/$nameFile.error
+        name=$(basename "$v" .deca)
+        nameFile=$name.$cTime
 
-        if [ -s $LOGS/$nameFile.output ]
-        # Check if the file is empty (ie no output by default)
-        then
-            pFailure "OUTPUT FILE NOT EMPTY"
-            pFailure "SHOW LOGS FOR $nameFile"
-            pFailure "$LOGS/$nameFile.error"
-            pFailure "$LOGS/$nameFile.output"
-            exit 1
-        fi
+        decac $name".deca" >$LOGS/$nameFile.output 2>$LOGS/$nameFile.error
 
-        if [ -s $LOGS/$nameFile.error ]
-        # Check if the file is empty (ie no output by default)
-        then
-            pSuccess "Error Find \t $v"
+        if (cat $LOGS/$nameFile.output | grep -q -e "$errorToFind" || cat $LOGS/$nameFile.error | grep -q -e "$errorToFind"); then
+            # Compare the error find with the error expected
+            pSuccess "ERROR FIND in COMPILATION \t $name"
             rm $LOGS/$nameFile.*
         else
-            pFailure "NO ERROR FOUND"
-            pFailure "SHOW LOGS FOR $nameFile"
-            pFailure "$LOGS/$nameFile.error"
-            pFailure "$LOGS/$nameFile.output"
-            exit 1
+
+            # Error not find at compile
+            # Try to execute
+
+            rm $LOGS/$nameFile.*
+            ima $name".ass" >$LOGS/$nameFile.output 2>$LOGS/$nameFile.error
+
+            if (cat $LOGS/$nameFile.output | grep -q -e "$errorToFind" || cat $LOGS/$nameFile.error | grep -q -e "$errorToFind"); then
+                # Compare the error find with the error expected
+                pSuccess "ERROR FIND in EXECUTION \t $name"
+                rm $LOGS/$nameFile.*
+                rm $name".ass"
+            else
+
+                pFailure "NO ERROR FOUND"
+                pFailure "SHOW LOGS FOR $name"
+                pFailure "$LOGS/$nameFile.error"
+                pFailure "$LOGS/$nameFile.output"
+                exit 1
+
+            fi
+
         fi
-    
+
     done
 
     cd ..
 
-done 
-
-
+done
 
 exit 0
