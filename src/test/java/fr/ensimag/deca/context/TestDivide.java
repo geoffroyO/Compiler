f0bc7eb019275bcbc1b93ec011ecc122a0edd2bf
Lucas Morin <Lucas.Morin@grenoble-inp.org>
2020-01-25 20:54:05 +0100

    nouveaux tests unitaires fonctionnels avec Mockito

--- src/test/java/fr/ensimag/deca/context/TestDivide.java
+++ src/test/java/fr/ensimag/deca/context/TestDivide.java
@@ -4,6 +4,9 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+import static org.mockito.Matchers.*;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -14,6 +17,7 @@ import fr.ensimag.deca.DecacCompiler;
 import fr.ensimag.deca.tree.AbstractExpr;
 import fr.ensimag.deca.tree.ConvFloat;
 import fr.ensimag.deca.tree.Divide;
+import fr.ensimag.deca.tree.IntLiteral;
 
 /**
  * Test for the Divide node using mockito, using @Mock and @Before annotations.
@@ -25,10 +29,8 @@ public class TestDivide {
 
     final Type INT = new IntType(null);
     final Type FLOAT = new FloatType(null);
+    final ConvFloat CONVFLOAT = new ConvFloat(new IntLiteral(4));
     
-    @Mock
-    AbstractExpr convexpr;
-
     @Mock
     AbstractExpr intexpr1;
     @Mock
@@ -44,45 +46,47 @@ public class TestDivide {
     public void setup() throws ContextualError {
         MockitoAnnotations.initMocks(this);
         compiler = new DecacCompiler(null, null);
-//        when(intexpr1.verifyExpr(compiler, null, null)).thenReturn(INT);
-//        when(intexpr2.verifyExpr(compiler, null, null)).thenReturn(INT);
-//        when(floatexpr1.verifyExpr(compiler, null, null)).thenReturn(FLOAT);
-//        when(floatexpr2.verifyExpr(compiler, null, null)).thenReturn(FLOAT);
+        when(intexpr1.verifyExpr(compiler, null, null)).thenReturn(INT);
+        when(intexpr2.verifyExpr(compiler, null, null)).thenReturn(INT);
+        when(floatexpr1.verifyExpr(compiler, null, null)).thenReturn(FLOAT);
+        when(floatexpr2.verifyExpr(compiler, null, null)).thenReturn(FLOAT);
+        when(intexpr1.verifyRValue(eq(compiler), eq(null), eq(null), any(FloatType.class))).thenReturn(CONVFLOAT);
+        when(intexpr2.verifyRValue(eq(compiler), eq(null), eq(null), any(FloatType.class))).thenReturn(CONVFLOAT);
     }
 
     @Test
     public void testIntInt() throws ContextualError {
         Divide t = new Divide(intexpr1, intexpr2);
         // check the result
-//        assertTrue(t.verifyExpr(compiler, null, null).isFloat());
+        assertTrue(t.verifyExpr(compiler, null, null).isFloat());
         // check that the mocks have been called properly.
-//        verify(intexpr1).verifyExpr(compiler, null, null);
-//        verify(intexpr2).verifyExpr(compiler, null, null);
+        verify(intexpr1).verifyExpr(compiler, null, null);
+        verify(intexpr2).verifyExpr(compiler, null, null);
     }
 
     @Test
     public void testIntFloat() throws ContextualError {
     	Divide t = new Divide(intexpr1, floatexpr1);
 //         check the result
-//        assertTrue(t.verifyExpr(compiler, null, null).isFloat());
+        assertTrue(t.verifyExpr(compiler, null, null).isFloat());
         // ConvFloat should have been inserted on the right side
-//        assertTrue(t.getLeftOperand() instanceof ConvFloat);
-//        assertFalse(t.getRightOperand() instanceof ConvFloat);
+        assertTrue(t.getLeftOperand() instanceof ConvFloat);
+        assertFalse(t.getRightOperand() instanceof ConvFloat);
         // check that the mocks have been called properly.
-//        verify(intexpr1).verifyExpr(compiler, null, null);
-//        verify(floatexpr1).verifyExpr(compiler, null, null);
+        verify(intexpr1).verifyExpr(compiler, null, null);
+        verify(floatexpr1).verifyExpr(compiler, null, null);
     }
 
     @Test
     public void testFloatInt() throws ContextualError {
     	Divide t = new Divide(floatexpr1, intexpr1);
         // check the result
-//        assertTrue(t.verifyExpr(compiler, null, null).isFloat());
+        assertTrue(t.verifyExpr(compiler, null, null).isFloat());
         // ConvFloat should have been inserted on the right side
-//        assertTrue(t.getRightOperand() instanceof ConvFloat);
-//        assertFalse(t.getLeftOperand() instanceof ConvFloat);
+        assertTrue(t.getRightOperand() instanceof ConvFloat);
+        assertFalse(t.getLeftOperand() instanceof ConvFloat);
         // check that the mocks have been called properly.
-//        verify(intexpr1).verifyExpr(compiler, null, null);
-//        verify(floatexpr1).verifyExpr(compiler, null, null);
+        verify(intexpr1).verifyExpr(compiler, null, null);
+        verify(floatexpr1).verifyExpr(compiler, null, null);
     }
 }
---
+++ src/test/java/fr/ensimag/deca/context/TestModulo.java
@@ -0,0 +1,78 @@
+package fr.ensimag.deca.context;
+
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+import static org.mockito.Matchers.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+import fr.ensimag.deca.DecacCompiler;
+import fr.ensimag.deca.tree.AbstractExpr;
+import fr.ensimag.deca.tree.ConvFloat;
+import fr.ensimag.deca.tree.Location;
+import fr.ensimag.deca.tree.UnaryMinus;
+
+/**
+ * Test for the Divide node using mockito, using @Mock and @Before annotations.
+ *
+ * @author Ensimag
+ * @date 01/01/2020
+ */
+public class TestModulo {
+
+    final Type INT = new IntType(null);
+    final Type FLOAT = new FloatType(null);
+    
+    @Mock
+    AbstractExpr intexpr1;
+
+    @Mock
+    AbstractExpr floatexpr1;
+
+
+    DecacCompiler compiler;
+    
+    @Before
+    public void setup() throws ContextualError {
+//        MockitoAnnotations.initMocks(this);
+//        compiler = new DecacCompiler(null, null);
+//        when(intexpr1.verifyExpr(compiler, null, null)).thenReturn(INT);
+//        when(floatexpr1.verifyExpr(compiler, null, null)).thenReturn(FLOAT);
+    }
+
+    @Test
+    public void testInt() throws ContextualError {
+//        UnaryMinus t = new UnaryMinus(intexpr1);
+        // check the result
+//        assertTrue(t.verifyExpr(compiler, null, null).isInt());
+        // check that the mocks have been called properly.
+//        verify(intexpr1, Mockito.times(1)).verifyExpr(compiler, null, null);
+    }
+
+    @Test
+    public void testFloat() throws ContextualError {
+//    	UnaryMinus t = new UnaryMinus(floatexpr1);
+    	// check the result
+//        assertTrue(t.verifyExpr(compiler, null, null).isFloat());
+        // check that the mocks have been called properly.
+//        verify(floatexpr1, Mockito.times(1)).verifyExpr(compiler, null, null);
+    }
+    
+    @Test
+    public void testGetOp() throws ContextualError {
+//    	UnaryMinus t = new UnaryMinus(floatexpr1);
+//    	UnaryMinus sp = spy(t);
+//    	sp.verifyExpr(compiler, null, null);
+//    	assert(sp.getOperand() == floatexpr1);
+    }
+}
