d47300cfa62ecb42c5286bbbaf7b6f90b99651b7
Geoffroy Oudoumanessah <geoffroy.oudoumanessah@grenoble-inp.org>
2020-01-27 13:31:38 +0100

    recorrection return

--- src/main/java/fr/ensimag/deca/tree/Return.java
+++ src/main/java/fr/ensimag/deca/tree/Return.java
@@ -2,6 +2,7 @@ package fr.ensimag.deca.tree;
 
 import java.io.PrintStream;
 
+import fr.ensimag.ima.pseudocode.instructions.BRA;
 import fr.ensimag.ima.pseudocode.instructions.RTS;
 import org.apache.commons.lang.Validate;
 
@@ -44,7 +45,7 @@ public class Return extends AbstractInst {
 	@Override
 	protected void codeGenInst(DecacCompiler compiler) {
 		expr.codeGenExpr(compiler, Register.R0); // on retourne le résultat dans R0
-		compiler.addInstruction(new RTS());
+		compiler.addInstruction(new BRA(compiler.getLabM().getEndCurrentLabel()));
 	}
 
 	@Override
